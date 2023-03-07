d5d0247ef4ef2ee12d6c9ff2c1d8540b2f3e7dd9
Diego de Sousa Araujo <diesgo.araujo@gmail.com>
2023-03-07 09:54:01 -0300

    Adding the exercicio_III

--- exercicio_II/src/main.py
+++ exercicio_II/src/main.py
@@ -4,14 +4,12 @@ import requests_cache
 from bs4 import BeautifulSoup
 
 linksVisitados = []
-index = 0
+
 requests_cache.install_cache('myCache')
+
 url = "https://www.bbc.com/portuguese"
+
 class BancoDeDados:
-    # urlDoSite: str
-    # numeroDeTermos: int
-    # numeroDeLinks: int
-    # pontuacao: int
     def __init__(self, urlDoSite):
         self.urlDoSite = urlDoSite
         self.numeroDeTermos = []
@@ -26,12 +24,9 @@ class BancoDeDados:
             (len(self.numeroDeLinks))
         )
         return self.pontuacao
-        
-arrayDeDados: BancoDeDados = []
 
-def isNotHTTP(link: str):
-    if link.find('http') is None and link.find('https') is None:
-        return True
+
+arrayDeDados: BancoDeDados = []
 
 
 def pegarCadeiaDeCaracteres(match, texto: str):
@@ -44,24 +39,25 @@ def pegarCadeiaDeCaracteres(match, texto: str):
             continue
         cadeia += texto[word]
     return cadeia
-    # texto[posicao - 20 : posicao] + palavra_chave + texto[posicao + len(palavra_chave) : posicao + len(palavra_chave) + 20]
+
 
 def printBancoDeDados(
     url: str, numeroDeTermos: int, numeroDeLinks: int, numeroDeReferencias: int, pontuacao: int
-    ):
+):
     print(
-            f"A URL do site é:{url}\n"
-            f"O número de termos:{numeroDeTermos}\n"
-            f"O número de links:{numeroDeLinks}\n"
-            f"O número de referencias:{numeroDeReferencias}\n"
-            f"O número de pontos:{pontuacao}\n"
-        )
+        f"A URL do site é:{url}\n"
+        f"O número de termos:{numeroDeTermos}\n"
+        f"O número de links:{numeroDeLinks}\n"
+        f"O número de referencias:{numeroDeReferencias}\n"
+        f"O número de pontos:{pontuacao}\n"
+    )
+
 
 def tratarLink(link: str):
     if link[0] == '#':
         return url
     elif link[0] == "/":
-        return url + link[1 : ]
+        return url + link[1:]
     else:
         return link
 
@@ -81,63 +77,52 @@ def search(key: str, url: str, depth: int):
         termosEncontrados.append(
             pegarCadeiaDeCaracteres(occurences, text)
         )
-    
+
     myBancoDeDados.numeroDeTermos = termosEncontrados
-    
+
     linksEncontrados = []
     numeroDeReferencias = 0
-
 
     for links in soup.find_all('a'):
 
         link = links.get('href')
-        
+
         if link is None or link == '':
             continue
 
         novoLink = tratarLink(link)
 
-
         linksEncontrados.append(novoLink)
 
     for link in linksEncontrados:
-        
+
         if link in linksVisitados:
             numeroDeReferencias += 1
 
         if depth > 0:
-        
+
             if link not in linksVisitados:
                 search(key, link, depth - 1)
     myBancoDeDados.numeroDeLinks = linksEncontrados
     myBancoDeDados.numeroDeReferencias = numeroDeReferencias
 
-    # if url not in linksVisitados:
-    #     myBancoDeDados.numeroDeTermos = termosEncontrados
-
-    # print(f'A url era: {url}')
-    # print(f'Os links encontrados foram: {linksEncontrados}')
-    # print(f'Os links visitados foram: {linksVisitados}')
-    # printBancoDeDados(
-    #     myBancoDeDados.urlDoSite, len(myBancoDeDados.numeroDeTermos), 
-    #     len(myBancoDeDados.numeroDeLinks), myBancoDeDados.numeroDeReferencias,
-    #     myBancoDeDados.calcularPontuacao()
-    # )
     linksVisitados.append(url)
 
     arrayDeDados.append(myBancoDeDados)
 
-def main():
-    search('news', url, 1)
 
-    newArrayDeDados = sorted(arrayDeDados, key= lambda dado : dado.calcularPontuacao(), reverse= True)
-    
+def main():
+    search('notícia', url, 1)
+
+    newArrayDeDados = sorted(arrayDeDados, key=lambda dado: dado.calcularPontuacao(), reverse=True)
+
     for dados in newArrayDeDados:
 
         printBancoDeDados(
-            dados.urlDoSite, len(dados.numeroDeTermos), 
+            dados.urlDoSite, len(dados.numeroDeTermos),
             len(dados.numeroDeLinks), dados.numeroDeReferencias,
             dados.calcularPontuacao()
         )
+
 
 main()
